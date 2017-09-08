a80160abbc7f5d35b8739e26feaeb8c75c3a290f
leonliu06 <leonliu06@gmail.com>
2017-09-09 00:37:59 +0800

    local variable table

---
+++ src/main/java/SlotTest1.java
@@ -0,0 +1,9 @@
+public class SlotTest1{
+	public static void main(String[] args){
+		{
+			byte[] placeholder = new byte[64 * 1024 * 1024];
+		}
+		System.gc();
+	}
+	
+}
---
+++ src/main/java/SlotTest2.java
@@ -0,0 +1,10 @@
+public class SlotTest2{
+	public static void main(String[] args){
+		{
+			byte[] placeholder = new byte[64 * 1024 * 1024];
+		}
+		int a = 0;
+		System.gc();
+	}
+	
+}
